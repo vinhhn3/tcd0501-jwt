ed35d5e37971ef8c044d8adf399952c158ed7048
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-12-15 13:44:12 +0700

    Connect to database

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { MONGO_URI } = process.env;
+
+mongoose.set("strictQuery", false);
+exports.connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to database ...");
+    })
+    .catch((error) => {
+      console.log("Error connecting");
+      console.log(error);
+      process.exit(1);
+    });
+};
