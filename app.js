7b10e791a7cceeaf0a4ab67e2ed069166f579e50
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-12-15 13:55:15 +0700

    Add User model

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ const app = express();
 
 app.use(express.json());
 
+// Import User context
+const User = require("./model/user");
+
+// Register
+app.post("/register", (req, res) => {});
+
+// Login
+app.post("/login", (req, res) => {});
 module.exports = app;
